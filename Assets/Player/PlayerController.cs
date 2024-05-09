c4d6b8b62fe3f3c746fda114900adcaeaa8133b2
Yanr10 <yanrodrigues.silva@hotmail.com>
2024-05-09 17:47:10 -0300

    colocando obstáculos e consertando erro

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -7,20 +7,37 @@ public class PlayerController : MonoBehaviour
     [Header("Lerp")]
     public Transform target;
     public float lerpSpeed = 1f;
+    public string TagToCheckEnemy = "Enemy";
 
     public float speed = 1f;
-    //private Vector3 _pos;
 
+    private Vector3 _pos;
+    private bool _canRun;
 
+    private void Start()
+    {
+        _canRun = true;
+    }
     void Update()
     {
-        var pos = target.position;
-        pos.y = target.position.y;
-        pos.z = target.position.z;
+        if (!_canRun) return;
 
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
 
-        transform.position = Vector3.Lerp(transform.position, pos, lerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == TagToCheckEnemy)
+        {
+            _canRun = false;
+        }
+    }
+
+
 }
